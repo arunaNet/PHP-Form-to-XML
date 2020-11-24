f7bcc00df0baa523d10b5c953c6e0de8207d44e9
arunaNet <development@arunanet.com>
2020-11-24 07:05:14 +0100

    first upload

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# Form-to-XML
-Speichere Anfragen aus einem HTML-Formular in eine XML-Datei.
+# PHP ``HTML-Formular in XML-Datei speichern``
+
+[![Donate](https://img.shields.io/badge/Donate-PayPal-green.svg)](https://www.paypal.me/PTMarkus)
+
+> Mit diesem Skript kannst du Daten aus einem Web-Formular in eine XML-Datei speichern. Dabei wird bei neuen Anfragen der Inhalt der Datei aktualisiert und nicht überschrieben.
+
+## Umfang
+
+- HTML-Formular
+- PHP Skript zum speichern der Daten in ein XML-File
+
+## Getestet mit
+
+| PHP       |
+| ----------|
+| 7.2+      |
---
+++ index.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html lang="de"> 
+<head>
+    <meta charset="UTF-8">
+    <title>arunaNet - XML Datei aus Formular</title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">	
+	<link href="assets/css/libs/bootstrap.min.css" rel="stylesheet">
+	<link href="assets/css/style.css" rel="stylesheet">
+</head>
+<body>
+	<div class="container">
+		<h2 class="display-4 pb-5 pt-5 text-center">HTML-Formular in XML-Datei speichern</h2>
+		<form action="" method="post">
+			<div class="row">
+			  <div class="form-group col-md-4">
+				<label for="SelectGender">Anrede</label>
+				<select name="gender" class="form-control" id="SelectGender">
+                    <option value="">Bitte wählen</option>
+				    <option value="Herr">Herr</option>
+				    <option value="Frau">Frau</option>
+				</select>
+			  </div>
+			</div>
+            <div class="row">
+                <div class="form-group col-md-6">
+                  <label for="InputName">Name</label>
+                  <input name="name" type="name" class="form-control" id="InputName">
+                </div>
+                <div class="form-group col-md-6">
+                  <label for="InputEmail">E-Mail-Adresse <span class="req">PFLICHTFELD</span></label>
+                  <input name="email" type="email" class="form-control" id="InputEmail" aria-describedby="emailHelp" required>
+                </div>
+            </div>
+			<div class="form-group">
+			  <label for="TextareaMessage">Nachricht</label>
+			  <textarea name="message" class="form-control" id="TextareaMessage" rows="3"></textarea>
+			</div>
+			<div class="form-group">
+				<input type="hidden" name="optin" value="0">
+				<input type="checkbox" id="opt-in" name="optin" value="1" class="form-check" required>
+				<label class="form-check-label" for="opt-in">
+                    <span class="req">PFLICHTFELD</span><br>Ich habe die Hinweise in der <a href="#">Datenschutzerkl&auml;rung</a> verstanden und stimme diesen hiermit zu.
+				</label>
+			</div>
+            <?php
+                error_reporting(0);
+                $xmlFile = 'formReport.xml';
+
+                if ( isset( $_POST['sendform']) && !empty($_POST['email']) && !empty($_POST['optin']) ) :
+
+                    $email = $_POST['email'];
+
+                    if ( preg_match("/^[^0-9][_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/", $email) ) :
+
+                        if (!file_exists($xmlFile)) :
+                            $xml = new DOMDocument('1.0', 'ISO-8859-1');
+                            $xml->formatOutput = true;
+                            $xml->preserveWhiteSpace = false;
+                            $base = $xml->createElement('formReport');
+                        else :
+                            $xml = new DOMDocument('1.0', 'ISO-8859-1');
+                            $xml->formatOutput = true;
+                            $xml->preserveWhiteSpace = false;
+                            $xml->load($xmlFile);
+                            $base = $xml->documentElement;
+                        endif;
+
+                        $xml->appendChild($base);
+
+                        $base->appendChild($xmlItem = $xml->createElement('submit'));
+
+                        $xmlItem->appendChild($xml->createElement('timestamp', date("Y-m-d H:i:s")));
+                        $xmlItem->appendChild($xml->createElement('gender', $_POST['gender']));
+                        $xmlItem->appendChild($xml->createElement('name', $_POST['name']));
+                        $xmlItem->appendChild($xml->createElement('email', $_POST['email']));
+                        $xmlItem->appendChild($xml->createElement('message', $_POST['message']));
+
+                        $xml->save($xmlFile);
+
+                        if($xml->save($xmlFile)) :
+                            echo '
+                                <div class="alert alert-success" role="alert">
+                                    Daten erfolgreich gespeichert!
+                                </div>
+                            ';
+                        else :
+                            echo '
+                                <div class="alert alert-danger" role="alert">
+                                    Daten konnten nicht gespeichert werden!
+                                </div>
+                            ';
+                        endif;
+                    else :
+                        echo '
+                            <div class="alert alert-danger" role="alert">
+                                Bitte eine korrekte E-Mail-Adresse eingeben!
+                            </div>
+                        ';
+                    endif;
+                endif;
+
+            ?>
+            <button type="submit" name="sendform" class="btn btn-dark">Absenden</button>
+		</form>
+	</div>
+</body>
+</html>
